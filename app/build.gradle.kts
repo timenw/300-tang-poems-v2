a5c180de9c34512617645f6e6c30cf928cde657d
root <root@nQb9PPVA7v.flapjacks.com>
2026-06-07 23:55:18 +0000

    Revert "Rename APK to 300poems, bump version to 2.0.0"
    This reverts commit 7f811ed79dad32f0bb77244a0035bdb19bdb9424.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,10 @@ android {
 
     defaultConfig {
         applicationId = "com.poem300"
-        archivesBaseName = "300poems"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
