c46453f3c75cfd61ff47301cccaedfd5532d005a
timenw <timenw@github.com>
2026-06-07 14:51:15 +0000

    Add release signing config and CI release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,10 +22,20 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = "poem300pass"
+            keyAlias = "poem300"
+            keyPassword = "poem300pass"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
